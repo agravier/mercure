69965094e7720ca0b09195a6e5985de128a00159
Alexandre Gravier <alexandre.gravier@gmail.com>
2010-09-20 23:31:36 +0800

    leiningen init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mercure "1.0.0-SNAPSHOT"
+  :description "Bot for the Google AI competition 2010"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ihodes/ai-contest-planet-wars-clj "1.0.0-SNAPSHOT"]]
+  :main "mercure/core")
---
+++ test/mercure/test/core.clj
@@ -0,0 +1,6 @@
+(ns mercure.test.core
+  (:use [mercure.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
